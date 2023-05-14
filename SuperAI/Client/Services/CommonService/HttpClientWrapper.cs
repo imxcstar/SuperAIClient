c42c6efdc4a0294696a04011ac54314c9d29bd82
xcssa <imxcssa@qq.com>
2023-05-15 00:16:55 +0800

    修改

---
+++ SuperAI/Client/Services/CommonService/HttpClientWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace SuperAI.Client.Services.CommonService
+{
+    public class HttpClientWrapper
+    {
+        private readonly HttpClient httpClient;
+
+        public HttpClientWrapper(HttpClient? httpClient = null)
+        {
+            this.httpClient = httpClient ?? new HttpClient();
+        }
+
+        public void AddDefaultRequestHeader(string key, string value)
+        {
+            httpClient.DefaultRequestHeaders.Add(key, value);
+        }
+
+        public async Task<string?> GetAsync(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("GET请求异常: {0}", e.Message);
+                return null;
+            }
+        }
+
+        public async Task<string?> PostAsync(string url, string jsonContent)
+        {
+            try
+            {
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("POST请求异常: {0}", e.Message);
+                return null;
+            }
+        }
+    }
+}
